bc2f3bf41d3871d233df26ec8646ae904746b980
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-01-16 18:56:02 +0100

    Multi-dimentional array practice

---
+++ Arrays/src/arrays/MultiDimentionalArrays.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class MultiDimentionalArrays {
+    public static void main(String[] args) {
+        //Examples:
+       int[][] myArray = new int[3][];
+       Object[] multiArray = new Object[3];
+       multiArray[0] = new String[]{"a", "b", "c"};
+       multiArray[1] = new String[3][];
+       multiArray[2] = new String[3][][]; //three-dimensional array
+    }
+}
